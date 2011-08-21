24be00fe0b45285d3de248ef289118c3a841ce2a
Víctor Berga <vbergae@gmail.com>
2011-08-21 13:47:32 +0200

    Added Criteria property and new initializer witch uses criteria

--- OpenLibraClient/OpenLibraClient/OpenLibraClient.h
+++ OpenLibraClient/OpenLibraClient/OpenLibraClient.h
@@ -1,16 +1,39 @@
+//  This file is part of the OpenLibraClient package.
 //
 //  OpenLibraClient.h
 //  OpenLibraClient
 //
-//  Created by Víctor Berga on 21/08/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+//  Created by Víctor Berga on 21/08/11 <victor@victorberga.com>.
+//  Copyright 2011 Víctor Berga. All rights reserved.
+// 
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
 
 #import <Foundation/Foundation.h>
 
 #import "Criteria.h"
 #import "Book.h"
 
-@interface OpenLibraClient : NSObject
+/**
+ OpenLibraClient is the main class to fetch requests to OpenLibra Service.
+ */
+@interface OpenLibraClient : NSObject {
+@private
+    Criteria *_criteria;
+}
+
+/**
+ Criteria to make a request to OpenLibra service.
+ */
+@property (nonatomic, retain) Criteria *criteria;
+
+/**
+ Initializer. 
+ Creates an instance of OpenLibraClient with the given Criteria.
+ 
+ @param Criteria Search criterium
+ @return OpenLibraclient Initialized client
+ */
+- (id)initWithCriteria:(Criteria *)criteria;
 
 @end
--- OpenLibraClient/OpenLibraClient/OpenLibraClient.m
+++ OpenLibraClient/OpenLibraClient/OpenLibraClient.m
@@ -1,23 +1,48 @@
+//  This file is part of the OpenLibraClient package.
 //
 //  OpenLibraClient.m
 //  OpenLibraClient
 //
-//  Created by Víctor Berga on 21/08/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+//  Created by Víctor Berga on 21/08/11 <victor@victorberga.com>.
+//  Copyright 2011 Víctor Berga. All rights reserved.
+// 
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
 
 #import "OpenLibraClient.h"
 
 @implementation OpenLibraClient
 
+@synthesize criteria = _criteria;
+
 - (id)init
 {
     self = [super init];
     if (self) {
-        // Initialization code here.
+        [self setCriteria:[[[Criteria alloc] init] autorelease]];
     }
     
     return self;
+}
+
+- (id)initWithCriteria:(Criteria *)criteria
+{
+    self = [super init];
+    if (self) {
+        [self setCriteria:criteria];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Memory Management
+
+- (void)dealloc
+{
+    [self setCriteria:nil];
+    
+    [super dealloc];
 }
 
 @end
