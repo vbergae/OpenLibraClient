6614900604537c48dd2d0cc4e1957f3b9fb653ad
Víctor Berga <vbergae@gmail.com>
2011-08-23 23:26:25 +0200

    Added SBJson to project

---
+++ OpenLibraClient/OpenLibraClient/OpenLibraClientDelegate.h
@@ -0,0 +1,13 @@
+//
+//  OpenLibraClientDelegate.h
+//  OpenLibraClient
+//
+//  Created by Víctor Berga on 23/08/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol OpenLibraClientDelegate <NSObject>
+
+@end
