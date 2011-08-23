cf4b208e31f91a0eea65c477e660afad9bc8416c
Víctor Berga <vbergae@gmail.com>
2011-08-23 23:27:28 +0200

    Created library header to import in other projects

--- OpenLibraClient/OpenLibraClient/OpenLibra.h
+++ OpenLibraClient/OpenLibraClient/OpenLibra.h
@@ -1,13 +1,15 @@
+//  This file is part of the OpenLibraClient package.
 //
 //  OpenLibra.h
 //  OpenLibraClient
 //
-//  Created by Víctor Berga on 23/08/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+//  Created by Víctor Berga on 23/08/11 <victor@victorberga.com>.
+//  Copyright 2011 Víctor Berga. All rights reserved.
+// 
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
 
-#import <Foundation/Foundation.h>
-
-@protocol OpenLibra <NSObject>
-
-@end
+#import "OpenLibraClient.h"
+#import "OpenLibraClientDelegate.h"
+#import "Criteria.h"
+#import "Book.h"
