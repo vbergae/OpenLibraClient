8d83baccadfe902df7c49a7e20941af6311a1ee5
Víctor Berga <vbergae@gmail.com>
2011-08-23 23:27:45 +0200

    Added OpenLibraClientDelegate methods

--- OpenLibraDesktop/OpenLibraDesktop/OpenLibraDesktopAppDelegate.h
+++ OpenLibraDesktop/OpenLibraDesktop/OpenLibraDesktopAppDelegate.h
@@ -1,14 +1,19 @@
+//  This file is part of the OpenLibraClient package.
 //
 //  OpenLibraDesktopAppDelegate.h
 //  OpenLibraDesktop
 //
-//  Created by Víctor Berga on 21/08/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+//  Created by Víctor Berga on 21/08/11 <victor@victorberga.com>.
+//  Copyright 2011 Víctor Berga. All rights reserved.
+// 
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
 
 #import <Cocoa/Cocoa.h>
+#import "OpenLibra.h"
 
-@interface OpenLibraDesktopAppDelegate : NSObject <NSApplicationDelegate> {
+@interface OpenLibraDesktopAppDelegate : NSObject 
+<NSApplicationDelegate, OpenLibraDelegate> {
     NSWindow *window;
 }
 
--- OpenLibraDesktop/OpenLibraDesktop/OpenLibraDesktopAppDelegate.m
+++ OpenLibraDesktop/OpenLibraDesktop/OpenLibraDesktopAppDelegate.m
@@ -1,13 +1,15 @@
+//  This file is part of the OpenLibraClient package.
 //
 //  OpenLibraDesktopAppDelegate.m
 //  OpenLibraDesktop
 //
-//  Created by Víctor Berga on 21/08/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+//  Created by Víctor Berga on 21/08/11 <victor@victorberga.com>.
+//  Copyright 2011 Víctor Berga. All rights reserved.
+// 
+//  For the full copyright and license information, please view the LICENSE
+//  file that was distributed with this source code.
 
 #import "OpenLibraDesktopAppDelegate.h"
-#import "OpenLibraClient.h"
 
 @implementation OpenLibraDesktopAppDelegate
 
@@ -16,9 +18,36 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
-    OpenLibraClient *cr = [[OpenLibraClient alloc] init];
+    OpenLibraClient *cr = [[[OpenLibraClient alloc] init] autorelease];
+    [cr setDelegate:self];
     //[cr.criteria setField:FieldId withValue:@"589"];
     [cr fetchRequest];
+}
+
+#pragma mark -
+#pragma mark OpenLibraDelegate;
+
+- (void)willStartConnection:(OpenLibraClient *)client
+{
+    NSLog(@"OpenLibraClient will start connection: %@", client);
+}
+
+- (void)openLibraClient:(OpenLibraClient *)client 
+     didStartConnection:(NSURLConnection *)connection
+{
+    NSLog(@"OpenLibraClient did start connection: %@", client);
+}
+
+- (void)openLibraClientDidFinishLoading:(OpenLibraClient *)client
+{
+    NSLog(@"OpenLibraClient did finish loading: %@", client);    
+    NSLog(@"Books: %@", [client books]);
+}
+
+- (void)openLibraClient:(OpenLibraClient *)client 
+didFailConnectionWithError:(NSError *)error
+{
+    NSLog(@"Connection error: %@", error);
 }
 
 @end
